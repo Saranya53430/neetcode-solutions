da4f31a7b67dd2a5c01ce8efab996bd1ddd63b39
Saranya <ssaranya53430@gmail.com>
2026-06-18 15:15:32 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        arr = set() #set stores only unique values
+        for num in nums: #This visits every element in the list one by one
+            if num in arr: #Check if Number Already Exists
+                return True
+            arr.add(num) #Add Number to Set
+        return False       
